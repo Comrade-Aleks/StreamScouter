d277c19e7135257de9dd09b0978e255728823811
Comrade Aleks <36338054+Comrade-Aleks@users.noreply.github.com>
2025-04-04 18:45:56 +0200

    Update CheckForUpdate.py

--- CheckForUpdate.py
+++ CheckForUpdate.py
@@ -4,7 +4,7 @@ from tkinter import messagebox
 import os
 import threading
 
-Version = "v2.6.0"
+Version = "v2.6.1"
 REPO_OWNER = "Comrade-Aleks"
 REPO_NAME = "StreamScouter"
 MUTE_FILE = "mute_update_notifications.txt"
